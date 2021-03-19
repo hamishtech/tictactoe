84d0a62880fb3698fd1b1f4e830f1f8a3b6cd07e
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-03-19 15:25:28 +0400

    final draft before AI

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const board = (() => {
 
   //create DOM to replicate arr
   const arrToDOM = () => {
+    tracker = document.createElement("div");
+    tracker.textContent = "Tracker:";
+    tracker.classList.add("tracker");
+    document.body.appendChild(tracker);
     let gameContainer = document.createElement("div");
     gameContainer.classList.add("gameContainer");
     gameContainer.dataset.turn = "player1";
@@ -18,11 +22,12 @@ const board = (() => {
         box.dataset.id = `${i}${j}`;
         box.dataset.i = i;
         box.dataset.j = j;
-        box.textContent = arr[i][j];
+        box.textContent = "";
         gameContainer.appendChild(box);
       }
     }
   };
+
   //updates DOM based on play function inputs
   const updateDOM = (r, c, marker) => {
     let box = document.querySelector(`[data-id="${r}${c}"]`);
@@ -31,11 +36,17 @@ const board = (() => {
 
   //play function which will be accessed by player Obj
   const play = (r, c, marker) => (
-    (arr[r][c] = marker), updateDOM(r, c, marker)
+    (arr[r][c] = marker),
+    updateDOM(r, c, marker),
+    turnsPlayed++,
+    updateTracker()
   );
 
-  //setting up players
-  let currentPlayer = 0;
+  //setting up players and boardStatus
+  let currentPlayer = 1;
+  let lastPlayer = 0;
+  let status = "";
+  let turnsPlayed = 0;
 
   const boxEventListener = () => {
     const boxes = document.querySelectorAll("[data-type='box']");
@@ -49,36 +60,120 @@ const board = (() => {
         }
         if (currentPlayer == 1) {
           playerOne.play(r, c);
+          lastPlayer = 1;
           currentPlayer = 2;
         } else {
           playerTwo.play(r, c);
+          lastPlayer = 2;
           currentPlayer = 1;
         }
       })
     );
   };
+
   //need write algorithm that checks for win/draw
   const checkStatus = () => {
-    if (arr[0][0] == "X" && arr[0][1] == "X" && arr[0][2] == "X") {
-      alert("game is won");
+    console.log(status, turnsPlayed);
+    let objDiagonal1 = { X: 0, O: 0 };
+    let objDiagonal2 = { X: 0, O: 0 };
+    for (let i = 0; i < arr.length; i++) {
+      let objHorizontal = { X: 0, O: 0 };
+      let objVertical = { X: 0, O: 0 };
+      if (arr[i][i] == "X" || arr[i][i] == "O") {
+        objDiagonal1[arr[i][i]]++;
+        if (objDiagonal1["X"] == 3 || objDiagonal1["O"] == 3) {
+          if (lastPlayer == 1) {
+            playerOne.win();
+          } else if (lastPlayer == 2) {
+            playerTwo.win();
+          }
+        }
+      }
+      let lastIndex = arr.length - 1;
+      if (arr[i][lastIndex - i] == "X" || arr[i][lastIndex - i] == "O") {
+        objDiagonal2[arr[i][lastIndex - i]]++;
+        if (objDiagonal2["X"] == 3 || objDiagonal2["O"] == 3) {
+          if (lastPlayer == 1) {
+            playerOne.win();
+          } else if (lastPlayer == 2) {
+            playerTwo.win();
+          }
+        }
+      }
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i][j] == "X" || arr[i][j] == "O") {
+          objHorizontal[arr[i][j]]++;
+          if (objHorizontal["X"] == 3 || objHorizontal["O"] == 3) {
+            if (lastPlayer == 1) {
+              playerOne.win();
+            } else if (lastPlayer == 2) {
+              playerTwo.win();
+            }
+          }
+        }
+      }
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j][i] == "X" || arr[j][i] == "O") {
+          objVertical[arr[j][i]]++;
+          if (objVertical["X"] == 3 || objVertical["O"] == 3) {
+            if (lastPlayer == 1) {
+              playerOne.win();
+            } else if (lastPlayer == 2) {
+              playerTwo.win();
+            }
+          }
+        }
+      }
+    }
+    if (turnsPlayed === 9 && board.status !== "won") {
+      alert("draw");
+      newGame();
     }
   };
-  const refresh = () => {
+
+  const updateTracker = () => {
+    let tracker = document.querySelector(".tracker");
+    if (currentPlayer == 1) {
+      current = "PlayerTwo";
+      marker = playerTwo.marker;
+    } else {
+      current = "PlayerOne";
+      marker = playerOne.marker;
+    }
+    tracker.textContent = `${current}, ${marker}`;
+  };
+
+  const emptyDOM = () => {
+    let gameGrid = document.querySelector(".gameContainer");
+    document.body.removeChild(gameGrid);
+    let tracker = document.querySelector(".tracker");
+    document.body.removeChild(tracker);
+  };
+
+  const newGame = () => {
     arr = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9],
     ];
-    console.table(arr);
+    emptyDOM();
+    arrToDOM();
+    boxEventListener();
+    currentPlayer = 1;
+    lastPlayer = 0;
+    status = "";
+    turnsPlayed = 0;
   };
   return {
-    arr,
     arrToDOM,
     play,
     currentPlayer,
     boxEventListener,
+    updateTracker,
+    newGame,
     checkStatus,
-    refresh,
+    status,
+    turnsPlayed,
   };
 })();
 
@@ -86,14 +181,18 @@ board.arrToDOM();
 board.boxEventListener();
 
 let Player = function (name, marker) {
-  if (board.currentPlayer == 0) {
-    board.currentPlayer = 1;
-  }
   const play = (r, c) => {
     board.play(r, c, marker);
     board.checkStatus();
   };
-  return { name, marker, play };
+
+  const win = () => {
+    board.status = "won";
+    alert(`${name} has won the game`);
+    board.newGame();
+  };
+
+  return { name, marker, play, win };
 };
 //function to make player play
 
